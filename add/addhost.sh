e63385abf3fe04a7f9e5a3e8f7dfd8d636c576e3
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-02-13 14:54:12 +0800

    Update addhost.sh

--- add/addhost.sh
+++ add/addhost.sh
@@ -13,7 +13,7 @@ LIGHT='\033[0;37m'
 # ==========================================
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$(curl -sS https://raw.githubusercontent.com/castleUI/ipvps/main/ip | awk '{print $4}' | grep $MYIP )
+IZIN=$(curl -sS https://raw.githubusercontent.com/Manpokr/mon/main/ipvps/ip | awk '{print $4}' | grep $MYIP )
 if [[ $MYIP = $IZIN ]]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
