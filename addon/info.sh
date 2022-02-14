1695dfac7c64f76dd02522f8df5b86f3e3e00dd9
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-02-14 22:21:59 +0800

    Update info.sh

--- addon/info.sh
+++ addon/info.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 clear
-neofetch --ascii_distro virus echo && echo "AutoScript By Manpok" &&  echo "t.me/Manpok" 
+neofetch --ascii_distro virus echo && echo "AutoScript By Manternet" &&  echo "t.me/Manternet" 
 
-echo "AutoScript VPN By Manpok"
-echo "t.me/Manpok"
+echo "AutoScript VPN By Manternet"
+echo "t.me/Manternet"
