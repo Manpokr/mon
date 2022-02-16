49b87c8caf5c6a645291975f2d434f87af03d66c
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-02-16 20:53:18 +0800

    Update addss.sh

--- add/addss.sh
+++ add/addss.sh
@@ -17,7 +17,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
 BURIQ () {
-    curl -sS  https://raw.githubusercontent.com/Manpokr/mon/main/ipvps/ip > /root/tmp
+    curl -sS  https://raw.githubusercontent.com/Manpokr/m/main/ip > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -35,7 +35,7 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/Manpokr/mon/main/ipvps/ip | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/Manpokr/m/main/ip | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -52,7 +52,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/Manpokr/mon/main/ipvps/ip | awk '{print $4}' | grep $MYIP )
+    IZIN=$(curl -sS https://raw.githubusercontent.com/Manpokr/m/main/ip | awk '{print $4}' | grep $MYIP )
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
