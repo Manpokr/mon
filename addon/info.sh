f64342c5748995709e3ec27c2203ca14e7f3264f
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-02-25 17:46:53 +0800

    Update info.sh

--- addon/info.sh
+++ addon/info.sh
@@ -3,5 +3,5 @@
 clear
 neofetch --ascii_distro virus echo && echo "AutoScript By Manternet" &&  echo "t.me/Manternet" 
 
-echo "AutoScript VPN By Manternet"
-echo "t.me/Manternet"
+cat /root/log-install.txt
+echo ""
