d44355d50cb503823f2f1ffb835c9c4e50145ce4
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-02-27 22:01:15 +0800

    Update menu-ssr.sh

--- menu/menu-ssr.sh
+++ menu/menu-ssr.sh
@@ -9,9 +9,9 @@ color2='\e[34;1m'
 color3='\e[0m'
 IP=$(wget -qO- ipinfo.io/ip);
 clear
-echo "╔═════════════════════════════════════════════════════════════════╗" | lolcat
-echo "║                       ┃ SSR / SS  Menu ┃                        ║"  | lolcat
-echo "╚═════════════════════════════════════════════════════════════════╝"  | lolcat
+echo "                        ╔═════════════╗" | lolcat
+echo "                        ┃ SSR / SS  Menu ┃ "  | lolcat
+echo "                        ╚═════════════╝"  | lolcat
 echo -e""
 echo -e "${color1}1${color3}.$bd  Create Account ShadowsocksR(${color2}addssr${color3})"
 echo -e "${color1}2${color3}.$bd  Create Account Shadowsocks  (${color2}addss${color3})"
