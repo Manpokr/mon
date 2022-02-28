41a507402e8f4cbca0f173d4404b911c81799ab2
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-03-01 00:05:03 +0800

    Update deltrgo.sh

--- del/deltrgo.sh
+++ del/deltrgo.sh
@@ -14,8 +14,6 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-exit 0
-fi
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/trojan-go/akun.conf")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
