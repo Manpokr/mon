76e7c5968335a3cde304349ab15d8c6aaeb850fe
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-03-04 19:02:33 +0800

    Update cert.sh

--- addon/cert.sh
+++ addon/cert.sh
@@ -4,7 +4,9 @@ domain=$(cat /root/domain)
 mkdir /root/.acme.sh
 curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
 chmod +x /root/.acme.sh/acme.sh
-~/.acme.sh/acme.sh --register-account -m Manpokr7@gmail.com
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256 ini
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+cd .acme.sh
+sudo bash acme.sh --register-account -m manpokr7@gmail.com
+sudo bash acme.sh --issue --standalone -d $domain  --force
+sudo bash acme.sh --installcert -d $domain --fullchainpath
+sudo bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
 rm -f /root/cert.sh
