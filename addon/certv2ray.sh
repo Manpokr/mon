7fa143e17384f92a0c0cc39aa81262b2a331bf01
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-03-04 20:21:20 +0800

    Update certv2ray.sh

--- addon/certv2ray.sh
+++ addon/certv2ray.sh
@@ -24,11 +24,11 @@ systemctl stop v2ray
 systemctl stop v2ray@none
 systemctl stop xray
 sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
-cd /root/
-wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/maste$bash acme.sh --install
-rm acme.sh
+clear
 cd .acme.sh
-echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"          bash acme.sh --register-account -m Manpokr7@gmail.com
+echo "starting....," 
+echo "Port 80 Akan di Hentikan Saat Proses install Cert"          
+bash acme.sh --register-account -m Manpokr7@gmail.com
 bash acme.sh --issue --standalone -d $domain --force
 bash acme.sh --installcert -d $domain --fullchainpath
 bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
