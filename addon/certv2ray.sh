fa6d078ba701719b962668ad24aa149f114deb8e
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-04-21 00:50:40 +0800

    Update certv2ray.sh

--- addon/certv2ray.sh
+++ addon/certv2ray.sh
@@ -15,8 +15,19 @@ LIGHT='\033[0;37m'
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
-MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$(curl -sS https://raw.githubusercontent.com/manternet/ipvps/main/ip | awk '{print $4}' | grep $MYIP )
+if [[ $MYIP = $IZIN ]]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+rm -f certv2ray.sh
+exit 0
+fi                                                                                                                                                     clear
 clear
+
 echo start
 sleep 0.5
 domain=$(cat /etc/xray/domain)
@@ -26,7 +37,7 @@ systemctl stop xray
 sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
 clear
 cd .acme.sh
-echo "starting....," 
+echo "${RED}starting....,${NC}" 
 echo "Port 80 Akan di Hentikan Saat Proses install Cert"    
 bash acme.sh --set-default-ca --server letsencrypt
 bash acme.sh --issue -d $domain --standalone -k ec-256 --force
@@ -40,3 +51,4 @@ echo Done
 sleep 0.5 
 clear
 neofetch
+rm -f certv2ray.sh
