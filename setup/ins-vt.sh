89a2c89819c8aeb14551c15b3fed57f4d4a0ea25
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-04-21 01:21:49 +0800

    Update ins-vt.sh

--- setup/ins-vt.sh
+++ setup/ins-vt.sh
@@ -1,11 +1,21 @@
- #!/bin/bash
- red='\e[1;31m'
+#!/bin/bash
+red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
+IZIN=$(curl -sS https://raw.githubusercontent.com/manternet/ipvps/main/ip | awk '{print $4}' | grep $MYIP )
+if [[ $MYIP = $IZIN ]]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+rm -f ins-vt.sh
+exit 0
+fi    
 clear
- #V2Ray Core
+
+#V2Ray Core
 domain=$(cat /etc/v2ray/domain)
 apt-get install netfilter-persistent -y
 apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
