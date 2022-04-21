3de1db92fd700cce9b89bb4f743f3ffefbc1f460
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-04-21 15:00:56 +0800

    Update menu-v2ray.sh

--- menu/menu-v2ray.sh
+++ menu/menu-v2ray.sh
@@ -2,16 +2,10 @@
 RED='\033[0;31m'
 NC='\033[0m'
 GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
 BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 NC='\033[0;37m'
-bd='\e[1m'
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-bl='\e[36;1m'
 bd='\e[1m'
 
 IP=$(wget -qO- ipinfo.io/ip);
