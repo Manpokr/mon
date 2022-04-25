0631c67d087511c016c36e065a3c865cc5aa5229
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-04-25 23:21:22 +0800

    Update trial-menu.sh

--- menu/trial-menu.sh
+++ menu/trial-menu.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0;37m'
+bd='\e[1m'
+
 clear
 echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 echo -e "${RED}          • MENU TRIAL •          ${NC}"
@@ -9,19 +19,18 @@ echo -e "[${CYAN}•1${NC}] $bd Create TRial XRay VMess Accounts ${NC}"
 echo -e "[${CYAN}•2${NC}] $bd Create TRial XRay VLess Accounts ${NC}"                    
 echo -e "[${CYAN}•3${NC}] $bd Create TRial XRay Trojan Accounts ${NC}"                    
 echo -e "[${CYAN}•4${NC}] $bd Create TRial XRay XTLS Accounts ${NC}"                          
-echo -e "[${CYAN}•5${NC}] $bd Create TRial XRay GRPC Accounts ${NC}"                          
-echo -e "[${CYAN}•6${NC}] $bd Create TRial V2Ray Vmess Websocket ${NC}"                         
-echo -e "[${CYAN}•7${NC}] $bd Create TRial V2Ray Vless Websocket ${NC}"                        
-echo -e "[${CYAN}•8${NC}] $bd Create Trial V2Ray Trojan ${NC}"                               
-echo -e "[${CYAN}•9${NC}] $bd Create TRial ShadowsocksR ${NC}"                             
-echo -e "[${CYAN}10${NC}] $bd Create TRial Shadowsocks ${NC}"                           
-echo -e "[${CYAN}11${NC}] $bd Create TRial SSTP Accounts ${NC}"                          
-echo -e "[${CYAN}12${NC}] $bd Create TRial PPTP Accounts ${NC}"                            
-echo -e "[${CYAN}13${NC}] $bd Create TRial L2TP Accounts ${NC}"                               
+echo -e "[${CYAN}•5${NC}] $bd Create TRial XRay GRPC Accounts ${NC}"    
+echo -e "[${CYAN}•6${NC}] $bd Create TRial XRAY TR-GRPC Accounts ${NC}"                          
+echo -e "[${CYAN}•7${NC}] $bd Create TRial XRAY TR-XTLS Accounts ${NC}"                            
+echo -e "[${CYAN}•8${NC}] $bd Create TRial V2Ray Vmess Websocket ${NC}"                         
+echo -e "[${CYAN}•9${NC}] $bd Create TRial V2Ray Vless Websocket ${NC}"                        
+echo -e "[${CYAN}10${NC}] $bd Create Trial V2Ray Trojan ${NC}"                               
+echo -e "[${CYAN}11${NC}] $bd Create TRial ShadowsocksR ${NC}"                             
+echo -e "[${CYAN}12${NC}] $bd Create TRial Shadowsocks ${NC}"                           
 echo -e""
 echo -e "[${RED}•x${NC}] ${RED} Menu${NC}"
 echo -e""
-read -p " silahkan masukkan nomor [1-13 or x] :  "  menu
+read -p " silahkan masukkan nomor [1-12 or x] :  "  menu
 case $menu in 
 1)
 trialxvmess
@@ -39,28 +48,25 @@ trialxtls
 trialgrpc
 ;;
 6)
-trialvmess 
+trialtrgrpc 
 ;;
 7)
-trialvless
+trialtrxtls
 ;;
 8)
-trialtrojan
+trialvmess
 ;;
 9)
-trialssr
+trialvless
 ;;
 10)
-trialss
+trialtrojan
 ;;
 11)
-trialsstp
+trialssr
 ;;
 12)
-trialpptp
-;;
-13)
-triall2tp
+trialss
 ;;
 x)
 exit
