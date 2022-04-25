ebaa59b65731fa065601bfd894adf5450320f7a6
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-04-25 23:40:28 +0800

    Update addhost.sh

--- add/addhost.sh
+++ add/addhost.sh
@@ -19,10 +19,20 @@ echo $domain > /etc/xray/domain
 cp /etc/xray/domain /root/domain
 cp /etc/xray/domain /etc/v2ray/domain
 echo start
+systemctl stop v2ray
+systemctl stop v2ray@none
+systemctl stop xray
 systemctl stop vmess-grpc
-/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+cd .acme.sh
+bash acme.sh --set-default-ca --server letsencrypt
+bash acme.sh --issue -d $domain --standalone -k ec-256 --force
+bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
 systemctl start vmess-grpc.service
+systemctl start xray.service
+systemctl start v2ray
+systemctl start v2ray@none
+systemctl start vmess-grpc
 echo Done
 sleep 1.5
 clear
