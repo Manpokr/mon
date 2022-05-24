6359271997666455b1dacd08409da9b2ce43bf9d
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-05-25 01:24:36 +0800

    Update menu-xray.sh

--- menu/menu-xray.sh
+++ menu/menu-xray.sh
@@ -12,6 +12,22 @@ bd='\e[1m'
 #getting
 IP=$(wget -qO- ipinfo.io/ip);
 clear
+ssr_folder="/usr/local/xray"
+check_pid(){
+        PID=`ps -ef |grep -v grep | grep /etc/xray/config.json |awk '{print $2}'`
+}
+
+if [[ -e ${ssr_folder} ]]; then
+                check_pid
+if [[ ! -z "${PID}" ]]; then
+echo -e "Current status: ${GREEN} INSTALLED${NC} & ${GREEN}RUNNING${NC}"
+else
+echo -e "Current status: ${GREEN} INSTALLED${NC} BUT ${RED}NOT RUNNING${NC}"
+fi
+cd "${ssr_folder}"
+else
+echo -e "Current status: ${RED}NOT INSTALL ${NC}"
+fi
 echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 echo -e "${RED}          • MENU XRAY •          ${NC}"
 echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
