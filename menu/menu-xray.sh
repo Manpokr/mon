264914a88e1ac82659f95f155a6bea8032f12cca
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-05-25 01:26:46 +0800

    Update menu-xray.sh

--- menu/menu-xray.sh
+++ menu/menu-xray.sh
@@ -28,9 +28,9 @@ cd "${ssr_folder}"
 else
 echo -e "Current status: ${RED}NOT INSTALL ${NC}"
 fi
-echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e "${RED}          • MENU XRAY •          ${NC}"
-echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "${RED}                     • MENU XRAY •          ${NC}"
+echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ${NC}"
 echo -e""
 echo -e "[${CYAN}•1${NC}] $bd Create Account Vmess TCP ${NC}"
 echo -e "[${CYAN}•2${NC}] $bd Create Account Vless TCP ${NC}"
