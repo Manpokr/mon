bbf6017866ab8d7d0166299afbc603524e805dbd
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-05-27 23:54:54 +0800

    Update ssh-vpn.sh

--- setup/ssh-vpn.sh
+++ setup/ssh-vpn.sh
@@ -2,9 +2,20 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
+
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
+IZIN=$(curl -sS https://raw.githubusercontent.com/manternet/ipvps/main/ip | awk '{print $4}' | grep $MYIP )
+if [[ $MYIP = $IZIN ]]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+rm -f kernel-updt.sh
+exit 0
+fi
 clear
+
 # ==================================================
 # initializing var
 export DEBIAN_FRONTEND=noninteractive
