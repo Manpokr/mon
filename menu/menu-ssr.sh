105053dad7acfd0e16123fec9449ab54564dd6e4
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-05-29 22:11:43 +0800

    Update menu-ssr.sh

--- menu/menu-ssr.sh
+++ menu/menu-ssr.sh
@@ -10,27 +10,24 @@ NC='\033[0;37m'
 bd='\e[1m'
 
 IP=$(wget -qO- ipinfo.io/ip);
-clear
 ssr_folder="/usr/local/shadowsocksr"
-check_pid(){
-        PID=`ps -ef |grep -v grep | grep server.py |awk '{print $2}'`
-}
+ssr=$(systemctl status ssrmu | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
 
 if [[ -e ${ssr_folder} ]]; then
-                check_pid
-if [[ ! -z "${PID}" ]]; then
-echo -e "Current status: ${GREEN}SSR INSTALLED${NC} & ${GREEN}RUNNING${NC}"
+if [[ $ssr == "running" ]]; then
+         status_ssr=" ${GREEN}SSR INSTALLED${NC} & ${GREEN}RUNNING${NC}"
 else
-echo -e "Current status: ${GREEN}SSR INSTALLED${NC} BUT ${RED}NOT RUNNING${NC}"
+         status_ssr=" ${GREEN}SSR INSTALLED${NC} BUT ${RED}NOT RUNNING${NC}"
 fi
 cd "${ssr_folder}"
 else
-echo -e "Current status: ${RED}SSR NOT INSTALL ${NC}"
+         status_ssr=" ${RED}SSR NOT INSTALL ${NC}"
 fi
-
-echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"                                       
-echo -e "${RED}              • MENU SSR / SS •          ${NC}"                                           
+clear
 echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "\E[0;100;31m              • MENU SSR / SS •             \E[0m"
+echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ${NC}"
+echo -e "Current status:$status_ssr "
 echo -e""                                                                                                           
 echo -e "[${CYAN}•1${NC}] $bd  Create Account ShadowsocksR ${NC}"
 echo -e "[${CYAN}•2${NC}] $bd  Create Account Shadowsocks  ${NC}"
@@ -41,6 +38,8 @@ echo -e "[${CYAN}•6${NC}] $bd  Renew Account Shadowsocks ${NC}"
 echo -e "[${CYAN}•7${NC}] $bd  Check User Login SSR & SS ${NC}"
 echo -e""
 echo -e "[${RED}•x${NC}] ${RED} Menu${NC}"
+echo -e""
+echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ${NC}"
 echo -e""
 read -p " Please Enter The Number  [1-8 or x] :  "  menu
 echo -e ""
