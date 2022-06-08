76addd20ee089412ac7a44c492fa609462406a29
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-09 02:43:12 +0800

    Update changeport.sh

--- addon/changeport.sh
+++ addon/changeport.sh
@@ -27,77 +27,44 @@ rm -f changeport.sh
 exit 0                                                                                                                            
 fi                                                                                                                                                                                                                                                  
 clear
-echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"                 
-echo -e "${RED}          • PORT CHANGER •          ${NC}"               
-echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "\033[5;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[m"
+echo -e "\033[30;5;47m                   ⇱ PORT CHANGER ⇲               \033[m"
+echo -e "\033[5;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[m"
 echo -e ""
-echo -e "[•1] Change Port OpenVPN"
-echo -e "[•2] Change Port Wireguard"
-echo -e "[•3] Change Port Vmess"
-echo -e "[•4] Change Port VLess"
-echo -e "[•5] Change Port Trojan"
-echo -e "[•6] Change Port Squid"
-echo -e "[•7] Change Port XRay VMess"
-echo -e "[•8] Change Port XRay VLess"
-echo -e "[•9] Change Port XRay Trojan"
-echo -e "[10] Change Port XRay XTLS"
-echo -e "[11] Change Port Xray GRPC"
-echo -e "[12] Change Port Trojan-go"
-echo -e "[13] Change Port Trojan GRPC"
-echo -e "[14] Change Port Trojan XTLS"
+echo -e "[${CYAN}•1${NC}] Change Port OpenVPN"
+echo -e "[${CYAN}•2${NC}] Change Port Wireguard"
+echo -e "[${CYAN}•3${NC}] Change Port Vmess"
+echo -e "[${CYAN}•4${NC}] Change Port VLess"
+echo -e "[${CYAN}•5${NC}] Change Port Trojan"
+echo -e "[${CYAN}•6${NC}] Change Port Squid"
+echo -e "[${CYAN}•7${NC}] Change Port XRay VMess"
+echo -e "[${CYAN}•8${NC}] Change Port XRay VLess"
+echo -e "[${CYAN}•9${NC}] Change Port XRay Trojan"
+echo -e "[${CYAN}10${NC}] Change Port XRay XTLS"
+echo -e "[${CYAN}11${NC}] Change Port Xray GRPC"
+echo -e "[${CYAN}12${NC}] Change Port Trojan-go"
+echo -e "[${CYAN}13${NC}] Change Port Trojan GRPC"
+echo -e "[${CYAN}14${NC}] Change Port Trojan XTLS"
 echo -e ""
-echo -e "[${RED}•x${NC}] ${RED}Exit${NC}"
+echo -e "[${RED}•x${NC}] ${RED}Menu${NC}"
 echo -e ""
-echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-read -p "Select From Options [ 1-12 ] : " port
+echo -e "\033[5;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[m"
+read -p "  silahkan masukkan nomor [1-14 or x] :  "  port
 case $port in
-1)
-portovpn
-;;
-2)
-portwg
-;;
-3)
-port-vmess
-;;
-4)
-port-vless
-;;
-5)
-port-trojan
-;;
-6)
-portsquid
-;;
-7)
-port-xws
-;;
-8)
-port-xvl 
-;;
-9)
-port-xtr 
-;;
-10)
-port-xtls 
-;;
-11)
-port-grpc
-;;
-12)
-port-trojan-go
-;;
-13)
-port-trgrpc
-;;
-14)
-port-trxtls
-;;
-x)
-exit
-;;
-*)
-echo "Please enter an correct number"
-changeport
-;;
+1) clear ; portovpn ; exit ;;
+2) clear ; portwg ; exit ;;
+3) clear ; port-vmess ; exit ;; 
+4) clear ; port-vless ; exit ;; 
+5) clear ; port-trojan ; exit ;; 
+6) clear ; portsquid ; exit ;; 
+7) clear ; port-xws ; exit ;;
+8) clear ; port-xvl ; exit ;; 
+9) clear ; port-xtr ; exit ;;
+10) clear ; port-xtls ; exit ;;
+11) clear ; port-grpc ; exit ;;
+12) clear ; port-trojan-go ; exit ;;
+13) clear ; port-trgrpc ; exit ;; 
+14) clear ; port-trxtls ; exit ;; 
+x) clear ; menu ;;
+*) echo -e "" ; echo "Boh salah tekan " ; sleep 1 ; changeport ;;
 esac
