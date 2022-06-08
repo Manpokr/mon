aecbfa45039e0b39b8fb09e4a746b22040c072ac
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-09 01:38:06 +0800

    Update running.sh

--- menu/running.sh
+++ menu/running.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 RED='\033[0;31m'
-red="\e[1;31m"
-green="\e[0;32m"
-NC="\e[0m"
-PURPLE='\033[0;35m'
+NC='\033[0m'
 GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
-NC='\033[0m'
+LIGHT='\033[0;37m'
+red="\e[1;31m"
+green="\e[0;32m"
+
 MYIP=$(curl -sS ipinfo.io/ip)
 
 #GETTING
