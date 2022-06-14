c64467449430d7c2267a66b22f58fc0bafba832c
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-14 23:26:51 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -268,7 +268,7 @@ echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
 echo "   - IPtables                : [ON]"  | tee -a log-install.txt
 echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
 echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot On 04.30 GMT +7" | tee -a log-install.txt
+echo "   - Autoreboot On 12.00 GMT +7" | tee -a log-install.txt
 echo "   - Autobackup Data" | tee -a log-install.txt
 echo "   - Restore Data" | tee -a log-install.txt
 echo "   - Auto Delete Expired Account" | tee -a log-install.txt
@@ -279,9 +279,6 @@ echo ""  | tee -a log-install.txt
 echo "   - Dev/Main                : "  | tee -a log-install.txt
 echo "   - Recode                  : Manternet" | tee -a log-install.txt
 echo "   - Telegram                : T.me/Manternet"  | tee -a log-install.txt
-echo "   - Instagram               : ~"  | tee -a log-install.txt
-echo "   - Whatsapp                : ~"  | tee -a log-install.txt
-echo "   - Facebook                : ~" | tee -a log-install.txt
 echo " Reboot 7 Sec"
 sleep 7
 clear
