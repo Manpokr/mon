c1e91fa346b07cc4002658638dafdb587c695694
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-14 22:45:10 +0800

    Update cekvmess.sh

--- cek/cekvmess.sh
+++ cek/cekvmess.sh
@@ -6,9 +6,12 @@
 RED="\e[1;31m"
 GREEN="\e[0;32m"
 NC="\e[0m"
-
-# Validate Your IP Address
+###################
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###################
 clear
+# Validate Your IP Address
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo -n > /tmp/other.txt
 data=( `cat /etc/v2ray/config.json | grep '^###' | cut -d ' ' -f 2`);
