b7b10c26bc84ef37c19a9fbf748b37070a982639
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-14 23:09:22 +0800

    Update renewtrojan.sh

--- renew/renewtrojan.sh
+++ renew/renewtrojan.sh
@@ -3,9 +3,16 @@
 # ===============
 
 # Color
-RED="\e[1;31m"
-GREEN="\e[0;32m"
-NC="\e[0m"
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+
+# ==========================================
+# Getting
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
 
 # Validate Your IP Address
 MYIP=$(wget -qO- ipinfo.io/ip);
