3b07f88fb571f8e5d67952d3cd16eb0d0118a263
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-14 22:44:02 +0800

    Update cekxray.sh

--- cek/cekxray.sh
+++ cek/cekxray.sh
@@ -4,9 +4,13 @@ RED='\033[0;31m'
 NC='\033[0m'
 GREEN='\033[0;32m'
 
+############
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###################
+clear
 ##########
 MYIP=$(wget -qO- ifconfig.me/ip);
-clear
 echo -n > /tmp/other.txt
 data=( `cat /etc/xray/xrayxtls.json | grep '^###' | cut -d ' ' -f 2`);                                                              echo "-------------------------------";
 echo "-----------------------------------------";
