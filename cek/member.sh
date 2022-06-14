83472bd3acc084dc480b3d960397e2efa1704649
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-14 22:16:53 +0800

    Update member.sh

--- cek/member.sh
+++ cek/member.sh
@@ -2,14 +2,9 @@
 # My Telegram : https://t.me/Manternet
 # ==========================================
 # Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
+RED="\e[1;31m"
+GREEN="\e[0;32m"
+NC="\e[0m"
 # ==========================================
 # Getting
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
