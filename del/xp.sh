ce43f2bb2fbfa707023f02d41b21242c0905b4ff
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-16 03:36:20 +0800

    Update xp.sh

--- del/xp.sh
+++ del/xp.sh
@@ -13,35 +13,6 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #######################
 MYIP=$(curl -sS ipv4.icanhazip.com)
 clear
-data=( `cat /var/lib/manpokr/data-user-l2tp | grep '^###' | cut -d ' ' -f 2`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-exp=$(grep -w "^### $user" "/var/lib/manpokr/data-user-l2tp" | cut -d ' ' -f 3)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-if [[ "$exp2" = "0" ]]; then
-sed -i "/^### $user $exp/d" "/var/lib/akbarstorevp/data-user-l2tp"
-sed -i '/^"'"$user"'" l2tpd/d' /etc/ppp/chap-secrets
-sed -i '/^'"$user"':\$1\$/d' /etc/ipsec.d/passwd
-chmod 600 /etc/ppp/chap-secrets* /etc/ipsec.d/passwd*
-fi
-done
-data=( `cat /var/lib/manpokr/data-user-pptp | grep '^###' | cut -d ' ' -f 2`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-exp=$(grep -w "^### $user" "/var/lib/manpokr/data-user-pptp" | cut -d ' ' -f 3)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-if [[ "$exp2" = "0" ]]; then
-sed -i "/^### $user $exp/d" "/var/lib/manpokr/data-user-pptp"
-sed -i '/^"'"$user"'" pptpd/d' /etc/ppp/chap-secrets
-chmod 600 /etc/ppp/chap-secrets*
-fi
-done
 data=( `cat /etc/shadowsocks-libev/akun.conf | grep '^###' | cut -d ' ' -f 2`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
@@ -76,19 +47,6 @@ cd
 fi
 done
 /etc/init.d/ssrmu restart
-data=( `cat /var/lib/manpokr/data-user-sstp | grep '^###' | cut -d ' ' -f 2`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-exp=$(grep -w "^### $user" "/var/lib/manpokr/data-user-sstp" | cut -d ' ' -f 3)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-if [[ "$exp2" = "0" ]]; then
-sed -i "/^### $user $exp/d" "/var/lib/manpokr/data-user-sstp"
-sed -i '/^'"$user"'/d' /home/sstp/sstp_account
-fi
-done
 data=( `cat /etc/xray/config.json | grep '^#&#' | cut -d ' ' -f 2`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
@@ -102,19 +60,6 @@ sed -i "/^#&# $user $exp/,/^},{/d" /etc/xray/config.json
 fi
 done
 systemctl restart xray.service
-data=( `cat /etc/trojan-go/akun.conf | grep '^###' | cut -d ' ' -f 2`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-exp=$(grep -w "^### $user" "/etc/trojan-go/akun.conf" | cut -d ' ' -f 3)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-if [[ "$exp2" = "0" ]]; then
-sed -i "/^### $user $exp/,/^},{/d" /etc/trojan-go/akun.conf
-fi
-done
-systemctl restart trojan-go
 data=( `cat /etc/wireguard/wg0.conf | grep '^### Client' | cut -d ' ' -f 3`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
