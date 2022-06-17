c82ed021672f6c94062f0f98e51f93ab52ed83dd
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-17 14:57:30 +0800

    Update xp.sh

--- del/xp.sh
+++ del/xp.sh
@@ -213,3 +213,52 @@ fi
 done
 systemctl restart xr-vl-ntls
 systemctl restart xr-vl-tls
+
+#################
+# V2ray
+data=( `cat /etc/v2ray/vless.json | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/etc/v2ray/vless.json" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+sed -i "/^### $user $exp/,/^},{/d"  /etc/v2ray/vless.json
+sed -i "/^### $user $exp/,/^},{/d" /etc/v2ray/vnone.json
+fi
+done
+systemctl restart v2ray@vless
+systemctl restart v2ray@vnone
+
+data=( `cat /etc/v2ray/config.json | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/etc/v2ray/config.json" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+sed -i "/^### $user $exp/,/^},{/d"  /etc/v2ray/config.json
+sed -i "/^### $user $exp/,/^},{/d" /etc/v2ray/none.json
+fi
+done
+systemctl restart v2ray
+systemctl restart v2ray@none
+
+data=( `cat /etc/trojan/akun.conf | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/etc/trojan/akun.conf" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+sed -i "/^### $user $exp/,/^},{/d"  /etc/trojan/config.json
+sed -i "/^### $user $exp/,/^},{/d" /etc/trojan/akun.conf
+fi
+done
+systemctl restart trojan
