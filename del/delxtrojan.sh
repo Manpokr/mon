a20d7eba61cc4fc4e7b07637c4f95523118b85e0
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-17 15:58:29 +0800

    Update delxtrojan.sh

--- del/delxtrojan.sh
+++ del/delxtrojan.sh
@@ -13,7 +13,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 MYIP=$(curl -sS ipv4.icanhazip.com)
 clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/etc/xray/config.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/etc/xray/trojan.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
@@ -25,7 +25,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/etc/xray/config.json")
 	echo " Press CTRL+C to return"
 	echo " ==============================="
 	echo "     No  Expired   User"
-	grep -E "^#&# " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^#&# " "/etc/xray/trojan.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
@@ -33,10 +33,10 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/etc/xray/config.json")
 			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
-user=$(grep -E "^#&# " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^#&# " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
-sed -i "/^#&# $user $exp/,/^},{/d" /etc/xray/config.json
-sed -i "/^#&# $user $exp/,/^},{/d" /etc/xray/config.json
+user=$(grep -E "^#&# " "/etc/xray/trojan.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^#&# " "/etc/xray/trojan.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+sed -i "/^#&# $user $exp/,/^},{/d" /etc/xray/trojan.json
+sed -i "/^#&# $user $exp/,/^},{/d" /etc/xray/trojan.json
 systemctl restart xray.service
 service cron restart
 clear
