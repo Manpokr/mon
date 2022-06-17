fa9a6f196f054658b1eaee778a5b4addc90d0aa3
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-18 00:48:22 +0800

    Create xptr.sh

---
+++ xptr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+IP=$(wget -qO- ipinfo.io/ip);
+hariini=`date +%d-%m-%Y`
+
+data=( `cat /etc/xray/trojan.json | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/etc/xray/trojan.json" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+sed -i "/^### $user $exp/,/^},{/d" /etc/xray/trojan.json
+fi
+done
