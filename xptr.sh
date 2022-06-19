fb594cfc1cd0cf3c9b1b7e6b2c763e3eb054e8fb
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-19 19:47:06 +0800

    Update xptr.sh

--- xptr.sh
+++ xptr.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+
+###########
 IP=$(wget -qO- ipinfo.io/ip);
 hariini=`date +%d-%m-%Y`
 
