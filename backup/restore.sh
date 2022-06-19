5f50ff72d18e4f89b790726b3a59429aa3cd726d
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-19 19:19:02 +0800

    Update restore.sh

--- backup/restore.sh
+++ backup/restore.sh
@@ -13,6 +13,7 @@ unzip backup.zip
 rm -f backup.zip
 sleep 1
 echo Start Restore
+
 cd /root/backup
 cp passwd /etc/
 cp group /etc/
@@ -22,8 +23,8 @@ cp -r wireguard /etc/
 cp chap-secrets /etc/ppp/
 cp passwd1 /etc/ipsec.d/passwd
 cp ss.conf /etc/shadowsocks-libev/ss.conf
-cp -r pakyavpn /var/lib/
-cp -r sstp /home/
+cp -r Manpokr /var/lib/
+cp -r xray /etc/
 cp -r trojan /etc/
 cp -r v2ray /etc/
 cp -r shadowsocksr /usr/local/
