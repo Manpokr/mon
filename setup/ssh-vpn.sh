a7b2a47fb31ea02a0eb0bdb7adba0a345b65e4b2
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-21 00:08:20 +0800

    Update ssh-vpn.sh

--- setup/ssh-vpn.sh
+++ setup/ssh-vpn.sh
@@ -150,15 +150,15 @@ echo "neofetch" >> .profile
 sleep 1
 echo -e "[ ${green}INFO$NC ] Install nginx" 
 
-apt -y install nginx
-cd
-# apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl
+#apt -y install nginx
+#cd
+apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 curl https://raw.githubusercontent.com/Manpokr/mon/main/addon/nginx.conf > /etc/nginx/nginx.conf
 curl https://raw.githubusercontent.com/Manpokr/mon/main/addon/vps.conf > /etc/nginx/conf.d/vps.conf
-#sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
-#useradd -m vps;
+sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
+useradd -m vps;
 mkdir -p /home/vps/public_html
 echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
 chown -R www-data:www-data /home/vps/public_html
