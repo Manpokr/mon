67cd4459299608aa550f7e1af8e23f95b2955f91
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-24 21:41:28 +0800

    Update ins-vt.sh

--- setup/ins-vt.sh
+++ setup/ins-vt.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
+RED='\e[1;31m'
+GREEN='\e[0;32m'
 NC='\e[0m'
+
+# // Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 IZIN=$(curl -sS https://raw.githubusercontent.com/manternet/ipvps/main/ip | awk '{print $4}' | grep $MYIP )
