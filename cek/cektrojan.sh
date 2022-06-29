4a7be6f613386125225537adb7a5d2f3c13bd1c9
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-30 01:02:26 +0800

    Update cektrojan.sh

--- cek/cektrojan.sh
+++ cek/cektrojan.sh
@@ -15,13 +15,14 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 # Validate Your IP Address
 clear
 MYIP=$(wget -qO- ipinfo.io/ip);
+tr=$(cat /etc/trojan/config.json | grep local_port | sed 's/local_//g' | sed 's/port//g' | sed 's/://g' | sed 's/,//g' | sed 's/"//g' | sed 's/   //g' | sed 's/  //g')
 data=( `cat /var/log/trojan.log | grep -w 'authenticated as' | awk '{print $7}' | sort | uniq`);
 echo "-------------------------------";
 echo "-----=[ Trojan User Login ]=-----";
 echo "-------------------------------";
 for akun in "${data[@]}"
 do
-data2=( `lsof -n | grep -i ESTABLISHED | grep trojan | awk '{print $9}' | cut -d':' -f2 | grep -w 445 | cut -d- -f2 | grep -v '>127.0.0.1' | sort | uniq | cut -d'>' -f2`);
+data2=( `lsof -n | grep -i ESTABLISHED | grep trojan | awk '{print $9}' | cut -d':' -f2 | grep -w $tr | cut -d- -f2 | grep -v '>127.0.0.1' | sort | uniq | cut -d'>' -f2`);
 echo -n > /tmp/iptrojan.txt
 for ip in "${data2[@]}"
 do
