49a48c307d621d68d2cbc9e2ac4bacdb948a4347
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-01 00:33:46 +0800

    Update deltrxtls.sh

--- del/deltrxtls.sh
+++ del/deltrxtls.sh
@@ -13,7 +13,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 MYIP=$(curl -sS ipv4.icanhazip.com)
 clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/etc/xray/trojanxtls.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/trojanxtls.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
@@ -25,7 +25,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/etc/xray/trojanxtls.json")
 	echo " Press CTRL+C to return"
 	echo " ==============================="
 	echo "     No  Expired   User"
-	grep -E "^#&# " "/etc/xray/trojanxtls.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^### " "/etc/xray/trojanxtls.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
@@ -33,10 +33,10 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#&# " "/etc/xray/trojanxtls.json")
 			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
-user=$(grep -E "^#&# " "/etc/xray/trojanxtls.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^#&# " "/etc/xray/trojanxtls.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
-sed -i "/^#&# $user $exp/,/^},{/d" /etc/xray/trojanxtls.json
-sed -i "/^#&# $user $exp/,/^},{/d" /etc/xray/trojanxtls.json
+user=$(grep -E "^### " "/etc/xray/trojanxtls.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/etc/xray/trojanxtls.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+sed -i "/^### $user $exp/,/^},{/d" /etc/xray/trojanxtls.json
+sed -i "/^### $user $exp/,/^},{/d" /etc/xray/trojanxtls.json
 systemctl restart trojanxtls
 service cron restart
 clear
@@ -47,4 +47,4 @@ echo "================================"
 echo "Username  : $user"
 echo "Expired   : $exp"
 echo "================================"
-echo "Script By Manternet"
+echo "ScriptMod By Manternet"
