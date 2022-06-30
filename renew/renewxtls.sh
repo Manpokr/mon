a5c4a436a2bbf862fff64266f6a51e17b6445418
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-06-30 23:56:34 +0800

    Update renewxtls.sh

--- renew/renewxtls.sh
+++ renew/renewxtls.sh
@@ -16,40 +16,42 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 # Validate Your IP Address
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
-CLIENT_001=$( grep -c -E "^#DEPAN " "/etc/xray/xrayxtls.json" )
-echo "    =================================================="
-echo "               LIST VLESS CLIENT ON THIS VPS"
-echo "    =================================================="
-grep -e "^#DEPAN " "/etc/xray/xrayxtls.json" | cut -d ' ' -f 2-8 | nl -s ') '
+CLIENT_001=$( grep -c -E "^### " "/etc/xray/xrayxtls.json" )
+echo "=================================================="
+echo "      LIST VLESS XTLS CLIENT ON THIS VPS"
+echo "=================================================="
+grep -e "^### " "/etc/xray/xrayxtls.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_002} -ge 1 && ${CLIENT_002} -le ${CLIENT_001} ]]; do
 		if [[ ${CLIENT_002} == '1' ]]; then
-                echo "    =================================================="
-			read -rp "    Please Input an Client Number (1-${CLIENT_001}) : " CLIENT_002
+                echo "=================================================="
+			read -rp "Please Input an Client Number (1-${CLIENT_001}) : " CLIENT_002
 		else
-                echo "    =================================================="
-			read -rp "    Please Input an Client Number (1-${CLIENT_001}) : " CLIENT_002
+                echo "=================================================="
+			read -rp "Please Input an Client Number (1-${CLIENT_001}) : " CLIENT_002
 		fi
 	done
-
-# // Clear
-clear
-clear && clear && clear
-clear;clear;clear
-
-# // Expired Date
-read -p "Expired  : " Jumlah_Hari
-
-user=$( grep -E "^#DEPAN " "/etc/xray/xrayxtls.json" | cut -d ' ' -f 4 | sed -n "${CLIENT_002}"p)
-exp=$( grep -E "^#DEPAN " "/etc/xray/xrayxtls.json" | cut -d ' ' -f 8 | sed -n "${CLIENT_002}"p)
-
-# // Date Configration
-now=$(date +%Y-%m-%d)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-exp3=$(($exp2 + $Jumlah_Hari))
-exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-
-# // Input To System Configuration
-sed -i "s/#DEPAN Username : $user | Expired : $exp/#DEPAN Username : $user | Expired : $exp4/g" /etc/xray/xrayxtls.json
-sed -i "s/#BELAKANG Username : $user | Expired : $exp/#BELAKANG Username : $user | Expired : $exp4/g" /etc/xray/xrayxtls.json
+# // Expired Date                                                                                                   
+read -p "Expired (Days) : " masaaktif                                                                               
+user=$(grep -E "^### " "/etc/xray/xrayxtls.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_002}"p)                       
+exp=$(grep -E "^### " "/etc/xray/xrayxtls.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_002}"p)                        
+                                                                                                                    
+# // Date Configration                                                                                              
+now=$(date +%Y-%m-%d)                                                                                               
+d1=$(date -d "$exp" +%s)                                                                                            
+d2=$(date -d "$now" +%s)                                                                                            
+exp2=$(( (d1 - d2) / 86400 ))                                                                                       
+exp3=$(($exp2 + $masaaktif))                                                                                        
+exp4=`date -d "$exp3 days" +"%Y-%m-%d"`                                                                             
+                      
+# // Input To System Configuration                                        
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/xrayxtls.json       
+systemctl restart xtls.service                                            
+clear                                                                     
+echo ""                                                                   
+echo "==============================="                                    
+echo "  Xray/Xtls  Account Renewed  "                                       
+echo "==============================="                                    
+echo "Username  : $user"                                                  
+echo "Expired   : $exp4"                                                                                    
+echo "================================"
+echo "ScriptMod By Manternet"
