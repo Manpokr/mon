bd5ccf13404a9974b03cabf20575d8b5fc30f4fb
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-01 02:02:09 +0800

    Update cekvless.sh

--- cek/cekvless.sh
+++ cek/cekvless.sh
@@ -52,6 +52,7 @@ oth=$(cat /tmp/other.txt | sort | uniq | nl)
 echo "other";
 echo "$oth";
 echo "-------------------------------"
+echo "ScriptMod By Manternet"
 rm -rf /tmp/other.txt
 read -p "Press Enter For Back To V2Ray/VLess Menu / CTRL+C To Cancel : "
 menu-v2ray
