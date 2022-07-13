6db6a8c4fb5a61b3ba13f3f3b28b8c06f5e4e73e
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-14 00:03:13 +0800

    Update delxtls.sh

--- del/delxtls.sh
+++ del/delxtls.sh
@@ -14,20 +14,27 @@ clear
 
 # // Getting V2Ray Client Data
 CLIENT_001=$(grep -c -E "^### " "/etc/xray/xrayxtls.json")
-echo "=================================================="
-echo "       LIST VLESS XTLS CLIENT ON THIS VPS"
-echo "=================================================="
-grep -e "^### " "/etc/xray/xrayxtls.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	if [[ ${CLIENTS_001} == '0' ]]; then
+		echo ""
+		echo "You have no existing clients!"
+		exit 1
+	fi
+
+	echo ""
+	echo " Select No client Xray Xtls"
+	echo " Press CTRL+C to return"
+	echo " ==============================="
+	echo "     No  User  Expired "
+        grep -e "^### " "/etc/xray/xrayxtls.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_002} -ge 1 && ${CLIENT_002} -le ${CLIENT_001} ]]; do
 		if [[ ${CLIENT_002} == '1' ]]; then
-                echo "=================================================="
-			read -rp "Please Input an Client Number (1-${CLIENT_001}) : " CLIENT_002
+               	echo " ==============================="
+                read -rp " Select one client (1-${CLIENT_001}) : " CLIENT_002
 		else
-                echo "=================================================="
-			read -rp "Please Input an Client Number (1-${CLIENT_001}) : " CLIENT_002
+                echo " ==============================="
+                read -rp " Select one client (1-${CLIENT_001}) : " CLIENT_002
 		fi
 	done
-
 # // String For Username && Expired Date
 user=$(grep -E "^### " "/etc/xray/xrayxtls.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_002}"p)                       
 exp=$(grep -E "^### " "/etc/xray/xrayxtls.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_002}"p)
@@ -44,7 +51,7 @@ clear
 # // Successfull
 echo ""
 echo "================================"
-echo "   Xray/Xtls Account Deleted   "
+echo "    Xray/Xtls Account Deleted   "
 echo "================================"
 echo "Username  : $user"
 echo "Expired   : $exp"
