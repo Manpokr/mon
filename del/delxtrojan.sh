f397d883ce5f026c6bc07ef9892f7121a73ba6f1
Manpokr <90512305+Manpokr@users.noreply.github.com>
2022-07-14 00:15:19 +0800

    Update delxtrojan.sh

--- del/delxtrojan.sh
+++ del/delxtrojan.sh
@@ -21,17 +21,19 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/trojan.json")
 	fi
 
 	echo ""
-	echo " Select the existing client you want to remove"
+	echo " Client Xray Trojan remove"
 	echo " Press CTRL+C to return"
 	echo " ==============================="
 	echo "     No  Expired   User"
 	grep -E "^### " "/etc/xray/trojan.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
-			read -rp "Select one client [1]: " CLIENT_NUMBER
-		else
-			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
-		fi
+	echo " ==============================="
+	read -rp "Select one client [1]: " CLIENT_NUMBER
+	else
+	echo " ==============================="
+	read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+	fi
 	done
 user=$(grep -E "^### " "/etc/xray/trojan.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
 exp=$(grep -E "^### " "/etc/xray/trojan.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
