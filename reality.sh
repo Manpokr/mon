f90d7169cf0a754a8cc37155a4cfce60d7315139
Manpokr <90512305+Manpokr@users.noreply.github.com>
2023-03-26 20:31:26 +0800

    Update reality.sh

--- reality.sh
+++ reality.sh
@@ -1,4 +1,4 @@
-cat >/usr/local/etc/xray/reality.json <<-EOF
+I'llcat >/usr/local/etc/xray/reality.json <<-EOF
 {
   "log" : {
     "access": "/var/log/xray/access.log",
@@ -265,3 +265,49 @@ Public key: LcdUsWdlXzj-6JXJnas-wqwkALEEZV-ZntxZ2UHN9hMv
         }
     }
 }
+
+
+lsof -n | grep -i ESTABLISHED | grep trojan-go | awk '{print $11}' | grep -w 1006 | cut -d: -f1,3
+clear
+# // Cek Ssh
+if [ -e "/var/log/auth.log" ]; then
+        LOG="/var/log/auth.log";
+fi
+if [ -e "/var/log/secure" ]; then
+        LOG="/var/log/secure";
+fi
+
+data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
+echo
+echo "==========-[ DROPBEAR USER LOGIN ]-===========";
+echo "  ID  |  USERNAME  |  IP ADDRESS";
+echo "==============================================";
+cat $LOG | grep -i dropbear | grep -i "Password auth succeeded" > /tmp/login-db.txt;
+for PID in "${data[@]}"
+do
+        cat /tmp/login-db.txt | grep "dropbear\[$PID\]" > /tmp/login-db-pid.txt;
+        NUM=`cat /tmp/login-db-pid.txt | wc -l`;
+        USER=`cat /tmp/login-db-pid.txt | awk '{print $10}'`;
+        IP=`cat /tmp/login-db-pid.txt | awk '{print $12}'`;
+        if [ $NUM -eq 1 ]; then
+                echo " $PID - $USER - $IP";
+                fi
+done
+
+echo
+echo "==========-[ OPENSSH USER LOGIN ]-============";
+echo "  ID  |  USERNAME  |  IP ADDRESS";
+echo "==============================================";
+cat $LOG | grep -i sshd | grep -i "Accepted password for" > /tmp/login-db.txt
+data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
+
+for PID in "${data[@]}"
+do
+        cat /tmp/login-db.txt | grep "sshd\[$PID\]" > /tmp/login-db-pid.txt;
+        NUM=`cat /tmp/login-db-pid.txt | wc -l`;
+        USER=`cat /tmp/login-db-pid.txt | awk '{print $9}'`;
+        IP=`cat /tmp/login-db-pid.txt | awk '{print $11}'`;
+        if [ $NUM -eq 1 ]; then
+                echo "$PID - $USER - $IP";
+        fi
+done
