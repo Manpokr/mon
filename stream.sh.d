951960dd765523c67f92df911c79111999f0fb87
Manpokr <90512305+Manpokr@users.noreply.github.com>
2023-07-15 01:56:34 +0800

    Update stream.sh

--- stream.sh.d
+++ stream.sh.d
@@ -3,7 +3,7 @@
 # ==================================================================
 # Name : VPN Script Quick Installation Script
 # Base : ****
-# Mod By : Manternet
+# Mod By : *****
 # ==================================================================
 
 # // Export Color & Information
@@ -33,8 +33,7 @@ echo -e "
   ${LIGHT}---------------------------------------------------${NC}
         ${PS1} CHECKING MEDIA STREAM UNLOCKER
          Time = $(date)
-
-         WELCOME TO MANTERNET VPN Script V2.0${LIGHT}
+${LIGHT}
   ---------------------------------------------------${LIGHT}";
 
 function InstallJQ() {
