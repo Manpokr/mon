117db2f29d53ca5287e2bbd36d43d7127b6c04a0
Manpokr <90512305+Manpokr@users.noreply.github.com>
2023-11-10 18:35:58 +0800

    Update xray-update.sh

--- xray-update.sh
+++ xray-update.sh
@@ -8,15 +8,21 @@
 # ==================================================================
 
 # // Export Color & Information
-export RED='\033[0;31m';
-export GREEN='\033[0;32m';
-export YELLOW='\033[0;33m';
-export BLUE='\033[0;34m';
-export PURPLE='\033[0;35m';
-export CYAN='\033[0;36m';
-export LIGHT='\033[0;37m';
-export GRAY='\033[0;38m';
-export NC='\033[0m';
+export NC='\e[0m'
+export GRAY='\e[1;30m'
+export RED='\e[0;31m'
+export GREEN='\e[0;32m'
+export BROWN='\e[0;33m'
+export BLUE='\e[0;34m'
+export PURPLE='\e[0;35m'
+export CYAN='\e[0;36m'
+export WHITE='\e[1;37m';
+export LIGHT_RED='\e[1;31m'
+export LIGHT_GREEN='\e[1;32m'
+export LIGHT_BLUE='\e[1;34m'
+export LIGHT_PURPLE='\e[1;35m'
+export LIGHT_CYAN='\e[1;36m'
+export LIGHT_GRAY='\e[0;37m'
 # // Export Banner Status Information
 export ERROR="(${RED} ERROR ${NC})";
 export INFO="(${CYAN} INFO ${NC})";
